6db4b0e8d3afb3f20a1a81b3baa8bc4da56a07e8
sarosir <sarosir.mc@gmail.com>
2026-03-09 14:54:54 +0100

    006

--- dynrelmap.js
+++ dynrelmap.js
@@ -10,16 +10,8 @@
 module.exports.dynrelmap = function (parent) {
     var obj = {};
     obj.parent = parent;
-    obj.meshServer = parent.parent;
-    obj.db = null;
-    obj.intervalTimer = null;
-    obj.debug = obj.meshServer.debug;
     obj.exports = [      
         'onDeviceRefreshEnd',
-        'resizeContent',
-        'historyData',
-        'variableData',
-        'malix_triggerOption'
     ];
     
     obj.onDeviceRefreshEnd = function() {
